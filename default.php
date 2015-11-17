b16606c2027e54a4bf4984683e462e1ed9e6b4f1
Efe Özgür <efecuneytozgur@gmail.com>
2015-11-17 22:10:23 +0200

    d
    d

---
+++ default.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<title>Ayrıntılı Kanun Şerhleri</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" href="css/ozel.css" />
+</head>
+<body>
+	<div class="col-md-3">
+		<div class="beyazkutu">
+			<p align="center">Ara</p> <input type="text" />
+			<div class="karakutu">
+				<p><input type="radio" name="ara" value="daireara">Daire Ara</p>
+				<p><input type="radio" name="ara" value="daireara">Kanun Ara</p>
+				<p><input type="radio" name="ara" value="daireara">Kanun Maddesi Ara</p>
+				<p><input type="radio" name="ara" value="daireara">Kanun Madde Metni Ara</p>
+				<p><input type="radio" name="ara" value="daireara" checked="checked">İçtihat Ara</p>
+				<p><input type="radio" name="ara" value="daireara">İçtihat Ara (Esasa Göre)</p>
+				<p><input type="radio" name="ara" value="daireara">İçtihat Ara (Karara Göre)</p>
+				<p><input type="submit" value="Ara" /></p>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
