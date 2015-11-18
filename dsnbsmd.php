e40bf261e6c5335f71101e86df033a5d0e15375d
Efe Özgür <efecuneytozgur@gmail.com>
2015-11-18 22:31:18 +0200

    d
    d

--- dsnbsmd.php
+++ dsnbsmd.php
@@ -4,7 +4,7 @@
 			echo "<table class='table table-hover'>";
 			while ($son5madde = mysqli_fetch_array($sonmaddeler)) {
 				extract($son5madde);
-				$temizmadde = substr(strip_tags($madde),0,200);
+				$temizmadde = substr(strip_tags($madde),0,200);   
 				echo "<tr><td><a title='$temizmadde ...devamı için tıklayın' href='dtekkanungoster.php?ilgili=$ilgiliserh'> $ilgiliserh </a> </td></tr>";
 				//echo "<p><a href='kanungoster.php#51'> $ilgiliserh </a> </p>";
 			}
