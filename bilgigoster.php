0129a64ee154f6bc9ccf56e9abcfc3645865f5c4
Efe Özgür <efecuneytozgur@gmail.com>
2015-11-20 12:56:54 +0200

    d
    d

--- bilgigoster.php
+++ bilgigoster.php
@@ -8,7 +8,7 @@
 	<link rel="stylesheet" href="css/ozel.css" />
 	<link href='https://fonts.googleapis.com/css?family=Noto+Sans' rel='stylesheet' type='text/css'>
 </head>
-<body>
+<body style="background-color: #272822;" >
 <?php include('dmenu.php'); ?>
 	<div class="tekkanunkutu">
 	<?php 
@@ -18,7 +18,11 @@
 		$bilgisorgula = mysqli_query($baglan, "select * from bilgitbl where bilgibaslik = '$bilgibaslik'");
 		while ($bilgi = mysqli_fetch_array($bilgisorgula)) {
 			extract($bilgi);
-			echo "<h4><a target='_blank' href='tekanungoster.php?ilgili=$bilgibaslik'>$bilgibaslik</a>. madde hakkında bilgi</h4><p style='font-size:10px;'><a href='bilgiedit.php?id=$id'>[Düzenle]</a></p><hr>"; 
+			echo "<h4><a target='_blank' href='tekanungoster.php?ilgili=$bilgibaslik'>$bilgibaslik</a>. madde hakkında bilgi</h4><p style='font-size:10px;'>";
+			if (@$_SESSION['kullanici'] == 'admin') {
+				echo "<a href='bilgiedit.php?id=$id'>[Düzenle]</a>";
+			}
+			echo "</p><hr>"; 
 			//$bilgi = strip_tags($bilgi);
 			echo "<p style='font-family:Noto Sans;font-size:10px;'>$bilgi</p>";
 		}
